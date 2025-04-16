0076b835f8d7e3bdb4cb18362b005a5c3871dca2
Junfang H <alyjh38@nottingham.ac.uk>
2025-04-16 14:06:43 +0100

    Update Illumina_bar05_annotation.sh

--- Illumina_bar05_annotation.sh
+++ Illumina_bar05_annotation.sh
@@ -5,17 +5,20 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100g
-#SBATCH --time=02:00:00
+#SBATCH --time=10:00:00
 #SBATCH --job-name=my_job
-#SBATCH --output=//share/BioinfMSc/rotation1/Group1/Illumina//bar5output/slurm-%x-%j.out
-#SBATCH --error=/share/BioinfMSc/rotation1/Group1/Illumina//bar5output/slurm-%x-%j.err
+#SBATCH --output=//share/BioinfMSc/rotation1/Group1/Illumina/bar5output_rerun/slurm-%x-%j.out
+#SBATCH --error=/share/BioinfMSc/rotation1/Group1/Illumina/bar5output_rerun/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-alyjh38@nottingham.ac.uk
+#SBATCH --mail-user=alyjh38@nottingham.ac.uk
 
 source $HOME/.bash_profile
 
 conda  activate prokka
 
-prokka assembly.fasta
+prokka --outdir /share/BioinfMSc/rotation1/Group1/Illumina/bar5output_rerun/prokka_bar05_v3 \
+       --prefix annotated_bar05 \
+       /share/BioinfMSc/rotation1/Group1/Illumina/bar5output_rerun/assembly_bar05.fasta
+
 
 conda deactivate
